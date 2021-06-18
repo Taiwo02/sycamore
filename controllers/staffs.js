c807268e8d03ddbcd65debd16ac906df2e6857d3
Taiwo Enoch <enochtaiwotimothy@gmail.com>
2021-06-18 21:54:39 +0100

    complete intigrate payment

--- controllers/staffs.js
+++ controllers/staffs.js
@@ -34,9 +34,9 @@ let staffs = {
             const result=await new User({firstname,lastname,email,address,password});
             result.save(function(error,response){
                 if(response && !error){ 
-                  const walletDetails =await new Wallet({user_id:response.id,balance:0,currency:"NGN",});
+                  const walletDetails = new Wallet({user_id:response.id,balance:0,currency:"NGN",});
                   walletDetails.save();
-                    res.status(200).send(response)
+                    res.status(200).send(walletDetails)
                 }
                 else{
                     res.status(500).send(error)
