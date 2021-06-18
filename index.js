6237e15f414b2e33d12994bd7e28268a87137439
Taiwo Enoch <enochtaiwotimothy@gmail.com>
2021-06-18 15:48:17 +0100

    done with create loan,get loan,change loan status,

--- index.js
+++ index.js
@@ -5,18 +5,18 @@ const routes = require('./routes/routes');
 const mongoose = require('mongoose');
 const mongoURL = process.env.MONGO_URL || 'mongodb://localhost/local'
 // mongoose.connect(mongoURL, { useNewUrlParser: true,useUnifiedTopology: true  });
-// mongoose.connect('mongodb://localhost/local', { useNewUrlParser: true,useUnifiedTopology: true  });
 // const db = mongoose.Connection;
 // db.on('err',(error)=> console.log(error));
 // db.once('open',()=>console.log('connected to database'));
 // mongoose.connect('mongodb+srv://Taiwo:08102637956@Ta@cluster0.lyrtx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', { useNewUrlParser: true,useUnifiedTopology: true,useCreateIndex:true  });
-const MongoClient = require('mongodb').MongoClient;
-const client = new MongoClient(mongoURL, { useNewUrlParser: true, useUnifiedTopology: true });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
+// const MongoClient = require('mongodb').MongoClient;
+// const client = new MongoClient(mongoURL, { useNewUrlParser: true, useUnifiedTopology: true });
+const client =  mongoose.connect(mongoURL, { useNewUrlParser: true,useUnifiedTopology: true  });
+// client.connect(err => {
+//   const collection = client.db("test").collection("devices");
+//   // perform actions on the collection object
+//   client.close();
+// });
 // mongoose.connect(mongoURL, { useNewUrlParser: true,useUnifiedTopology: true });
 
 app.use(bodyparser.json());
